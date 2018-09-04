5be2f3abd45fe2db4ef954dc6c1f2df4c46232a9
bin <17326852857@163.com>
2018-09-04 14:50:18 +0800

    9.4data修改

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,50 +1,359 @@
 <template>
-  <Row>
-    <Col span="5">
-      <DatePicker type="date" :options="startTimeOptions" @on-change="startTimeChange" placeholder="开始时间"
-                  v-model="starttime"></DatePicker>
-    </Col>
-    <Col span="5">
-      <DatePicker type="date" :options="endTimeOptions" @on-change="endTimeChange" placeholder="结束时间"
-                  v-model="endtime"></DatePicker>
-    </Col>
-  </Row>
+  <div class="layout">
+    <Layout>
+      <Header :style="{padding: 0}" class="layout-header-bar" ref="side1">
+        <div class="header">
+          <!--<img src="../assets/img/logo.png" class="logo"/>-->
+          <img v-show="isCollapsed" src="../assets/img/logo-min.png" key="min-logo" class="logomin"/>
+          <img v-show="!isCollapsed" src="../assets/img/logo.png" key="max-logo" class="logo"/>
+          <div class="line">
+            <Icon type="md-list" size="23" color="rgba(0,0,0,0.65)" @click.native="collapsedSider" :class="rotateIcon"/>
+          </div>
+          <div class="font">
+            <span>营收中心</span><span>Revenue Center</span>
+          </div>
+          <div class="user">
+            <img src="../assets/img/headIcon.jpg" class="head-icon"/>
+            <span>木杉</span>
+          </div>
+          <Dropdown class="pull-down-list">
+            <a href="javascript:void(0)">
+              角色
+              <Icon type="ios-arrow-down"></Icon>
+            </a>
+            <DropdownMenu slot="list" >
+              <DropdownItem>驴打滚</DropdownItem>
+              <DropdownItem>炸酱面</DropdownItem>
+              <DropdownItem disabled>豆汁儿</DropdownItem>
+              <DropdownItem>冰糖葫芦</DropdownItem>
+              <DropdownItem divided>北京烤鸭</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <Button type="text" class="esc">退出</Button>
+        </div>
+      </Header>
+      <Layout :style="{'margin-top': '0.07rem'}">
+        <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed" accordion>
+          <Menu theme="light" width="auto" :class="menuitemClasses" :style="{height: sideHeight + 'px'}" accordion v-show=" isCollapsed">
+            <MenuItem name="1-1" >
+              <Dropdown >
+              <a href="javascript:void(0)">
+              <Icon type="ios-desktop-outline" />
+              <span>系统管理</span>
+              </a>
+              <DropdownMenu slot="list" class="dropdown">
+                  <DropdownItem @click.native="gotoAddress('/user')">用户管理</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/role')">角色管理</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/menu')">菜单管理</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/request')">请求管理</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/task')">任务调度</DropdownItem>
+              </DropdownMenu>
+              </Dropdown>
+            </MenuItem>
+            <MenuItem name="1-2">
+              <Dropdown >
+                <a href="javascript:void(0)">
+                  <Icon type="ios-stats" />
+                  <span>基础数据</span>
+                </a>
+                <DropdownMenu slot="list" class="dropdown">
+                  <DropdownItem @click.native="gotoAddress('/legal')">法人主体</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/exchange')">汇率</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/financing')">金融机构</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/counterparty')">交易方</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/dictionary')">数据字典</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </MenuItem>
+            <MenuItem name="1-3">
+              <Dropdown >
+                <a href="javascript:void(0)">
+                  <Icon type="ios-card" />
+                  <span>收款管理</span>
+                </a>
+                <DropdownMenu slot="list" class="dropdown">
+                  <DropdownItem @click.native="gotoAddress('/gather')">收款管理</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </MenuItem>
+            <MenuItem name="1-4">
+              <Dropdown >
+                <a href="javascript:void(0)">
+                  <Icon type="ios-calculator-outline" />
+                  <span>报表管理</span>
+                </a>
+                <DropdownMenu slot="list" class="dropdown" :style="{'font-size':'0.13rem'}">
+                  <DropdownItem @click.native="gotoAddress('/ourAgent')">我方代收款明细表</DropdownItem >
+                  <DropdownItem @click.native="gotoAddress('/otherAgent')">交易方代收款明细表</DropdownItem>
+                  <DropdownItem @click.native="gotoAddress('/recognition')">认款明细表</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </MenuItem>
+          </Menu>
+          <Menu theme="light" width="auto" :class="menuitemClasses" :style="{height: sideHeight + 'px'}" accordion v-show="!isCollapsed">
+            <!--<Submenu name="1" >-->
+            <!--<template slot="title">-->
+              <!--<Icon type="ios-desktop-outline" />-->
+              <!--<span>系统管理</span>-->
+            <!--</template>-->
+              <!--<MenuItem name="1-1" @click.native="gotoAddress('/user')">用户管理</MenuItem>-->
+              <!--<MenuItem name="1-2" @click.native="gotoAddress('/role')">角色管理</MenuItem>-->
+              <!--<MenuItem name="1-3" @click.native="gotoAddress('/menu')">菜单管理</MenuItem>-->
+              <!--<MenuItem name="1-4" @click.native="gotoAddress('/request')">请求管理</MenuItem>-->
+              <!--<MenuItem name="1-5" @click.native="gotoAddress('/task')">任务调度</MenuItem>-->
+            <!--</Submenu>-->
+            <!--<Submenu name="2">-->
+              <!--<template slot="title">-->
+                <!--<Icon type="ios-stats" />-->
+                <!--<span>基础数据</span>-->
+              <!--</template>-->
+              <!--<MenuItem name="2-1" @click.native="gotoAddress('/legal')">法人主体</MenuItem>-->
+              <!--<MenuItem name="2-2" @click.native="gotoAddress('/exchange')">汇率</MenuItem>-->
+              <!--<MenuItem name="2-3" @click.native="gotoAddress('/financing')">金融机构</MenuItem>-->
+              <!--<MenuItem name="2-4" @click.native="gotoAddress('/counterparty')">交易方</MenuItem>-->
+              <!--<MenuItem name="2-5" @click.native="gotoAddress('/dictionary')">数据字典</MenuItem>-->
+            <!--</Submenu>-->
+            <!--<Submenu name="3">-->
+              <!--<template slot="title">-->
+                <!--<Icon type="ios-card" />-->
+                <!--<span>收款管理</span>-->
+              <!--</template>-->
+              <!--<MenuItem name="3-1" @click.native="gotoAddress('/gather')">收款管理</MenuItem>-->
+            <!--</Submenu>-->
+            <!--<Submenu name="4">-->
+              <!--<template slot="title">-->
+                <!--<Icon type="ios-calculator-outline" />-->
+                <!--<span>报表管理</span>-->
+              <!--</template>-->
+              <!--<MenuItem name="4-1" @click.native="gotoAddress('/ourAgent')">我方代收款明细表</MenuItem>-->
+              <!--<MenuItem name="4-2" @click.native="gotoAddress('/otherAgent')">交易方代收款明细表</MenuItem>-->
+              <!--<MenuItem name="4-3" @click.native="gotoAddress('/recognition')">认款明细表</MenuItem>-->
+            <!--</Submenu>-->
+            <Submenu v-for="item in menuData" :name="item.name">
+              <template slot="title">
+                <Icon :type="item.icon"></Icon><span>{{item.label}}</span>
+              </template>
+              <menu-item v-for="mi in item.menuitems" :name="mi.name" @click.native="mi.click">{{mi.label}}</menu-item>
+            </Submenu>
+          </Menu>
+        </Sider>
+        <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "test",
-      data() {
-        return {
-          startTimeOptions: {}, //开始日期设置
-          endTimeOptions: {}, //结束日期设置
-          starttime: '2018, 6, 14', //开始日期model
-          endtime: '2018, 8, 29',//结束日期model
-        }
+  export default {
+    name:'test',
+    data () {
+      return {
+        menuData:[
+          {   label:'系统管理',name:"m1",icon:"ios-desktop-outline",
+            menuitems:[
+              {name:'m1-1',label:'用户管理',click:()=>{gotoAddress('/user')}},
+              {name:'m1-2',label:'角色管理',click:"gotoAddress('/role')"},
+              {name:'m1-3',label:'菜单管理',click:'gotoAddress(\'/menu\')'},
+              {name:'m1-4',label:'请求管理',click:'gotoAddress(\'/request\')'},
+              {name:'m1-5',label:'任务调度',click:'gotoAddress(\'/task\')'},
+            ]
+          },
+          {   label:'基础数据',name:"m2",icon:'ios-stats',
+            menuitems:[
+              {name:'m2-1',label:'法人主体',url:'http://wallimn.iteye.com'},
+              {name:'m2-2',label:'汇率',url:'m2-1.html'},
+              {name:'m2-3',label:'金融机构',url:'m2-1.html'},
+              {name:'m2-4',label:'交易方',url:'m2-1.html'},
+              {name:'m2-4',label:'数据字典',url:'m2-1.html'},
+            ]
+          },
+          {   label:'收款管理',name:"m3",icon:'ios-card',
+            menuitems:[
+              {name:'m3-1',label:'收款管理',url:'m3-1.html'},
+            ]
+          },
+          {   label:'报表管理',name:"m4",icon:'ios-calculator-outline',
+            menuitems:[
+              {name:'m4-1',label:'我方代收款明细表',url:'m4-1.html'},
+              {name:'m4-2',label:'交易方代收款明细表',url:'m4-1.html'},
+              {name:'m4-3',label:'认款明细表',url:'m4-1.html'},
+            ]
+          }
+        ],
+        i1:false,
+        isCollapsed: false,
+      sideHeight: document.documentElement.clientHeight - 64,
+      }
+    },
+    computed: {
+      rotateIcon () {
+        return [
+          'menu-icon',
+          this.isCollapsed ? 'rotate-icon' : ''
+        ];
       },
-      methods: {
-        startTimeChange: function(e) { //设置开始时间
-          this.starttime = e;
-          this.endTimeOptions = {
-            disabledDate: date => {
-              let startTime = this.starttime ? new Date(this.starttime).valueOf() : '';
-              return date && (date.valueOf() < startTime);
-            }
-          }
-        },
-        endTimeChange: function(e) { //设置结束时间
-          this.endtime = e;
-          let endTime = this.endtime ? new Date(this.endtime).valueOf() - 1 * 24 * 60 * 60 * 1000 : '';
-          this.startTimeOptions = {
-            disabledDate(date) {
-              return date && date.valueOf() > endTime;
-            }
-          }
-        },
+      menuitemClasses () {
+        return [
+          'menu-item',
+          this.isCollapsed ? 'collapsed-menu' : ''
+        ]
+      }
+    },
+    methods: {
+      collapsedSider () {
+        this.$refs.side1.toggleCollapse();
+      },
+      gotoAddress (path) {
+        this.$router.push(path);
       }
     }
+  }
 </script>
+<style scoped >
+  .dropdown{
+    position: absolute;
+    left:0.35rem;
+    top:-0.4rem;
+    background: white;
+    z-index: 100;
+  }
+  .layout{
+    border: 0.01rem solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 0.04rem;
+    overflow: hidden;
+  }
+  .layout-header-bar{
+    background: #fff;
+    box-shadow: 0 0.01rem 0.01rem rgba(0,0,0,.1);
+  }
+  .layout-logo-left{
+    width: 90%;
+    height: 0.3rem;
+    background: #5b6270;
+    border-radius: 0.03rem;
+    margin: 15px auto;
+  }
+  .menu-icon{
+    transition: all .3s;
+  }
+  .rotate-icon{
+    transform: rotate(-90deg);
+  }
+  .menu-item span{
+    display: inline-block;
+    overflow: hidden;
+    width: 0.69rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    vertical-align: bottom;
+    transition: width .2s ease .2s;
+  }
+  .menu-item i{
+    transform: translateX(0rem);
+    transition: font-size .2s ease, transform .2s ease;
+    vertical-align: middle;
+    font-size: 0.14rem;
+  }
+  .collapsed-menu span{
+    width: 0rem;
+    transition: width .2s ease;
+  }
+  .collapsed-menu i{
+    transform: translateX(5px);
+    transition: font-size .2s ease .2s, transform .2s ease .2s;
+    vertical-align: middle;
+    font-size: 0.22rem;
+  }
 
-<style scoped>
+
+  .header{
+    width: 14.4rem;
+    height:0.64rem;
+    background:#fff;
+    position: relative;
+    border-bottom: 0.02rem solid #f8f8f8;
+  }
+  .logo{
+    width:1.06rem;
+    height: 0.27rem;
+    position: absolute;
+    left:0.28rem;
+    top:0.15rem;
+  }
+  .logomin{
+    /*width:1.06rem;*/
+    height: 0.27rem;
+    width:auto;
+    position: absolute;
+    left:0.22rem;
+    top:0.15rem;
+  }
+  .line{
+    position: absolute;
+    left:1.87rem;
+    top:0.05rem;
+  }
+  .font {
+    position: absolute;
+    left: 2.57rem;
+    top: 0.05rem;
+    color: #000;
+    letter-spacing: 0;
+    font-size: 0.14rem;
+    text-align: left;
+    opacity: 0.65;
+  }
+  .font span:first-child{
+    font-size: 0.14rem;
+    font-family: "PingFang SC",sans-serif;
+    }
+  .font span:last-child{
+    font-size: 0.14rem;
+    margin-left: 0.1rem;
+    font-family: Arial,sans-serif;
+    }
+
+  .pull-down-list{
+    position: absolute;
+    right:0.91rem;
+    top:0.06rem;
+    opacity:0.65;
+    font-size: 0.14rem;
+  }
+  .esc{
+    position: absolute;
+    right:0.31rem;
+    top:0.15rem;
+    opacity:0.65;
+    font-size: 0.14rem;
+  }
+
+  .user .head-icon{
+    position: absolute;
+    right:2.17rem;
+    top:0.18rem;
+    width: 0.24rem;
+    height:0.24rem;
+    border-radius: 0.12rem;
+    }
+  .user span{
+    position: absolute;
+    right:1.79rem;
+    top:0.05rem;
+    font-size: 0.14rem;
+    opacity:0.65;
+    }
+  .ivu-menu{
+    font-size:0.14rem;
+  }
+  .ivu-menu-item{
+    font-size: 0.12rem;
+  }
+  .ivu-dropdown-item{
+    font-size: 0.13rem;
+  }
 
 </style>
